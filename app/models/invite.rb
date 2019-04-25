4f357412ae4cf0342ecaa5729b7467e21b36a214
Nathan Cox <tsuujin@gmail.com>
2019-04-25 10:53:58 -0700

    Remove failing call to before_save on:
    As of Rails 5, before_save ignored on:
    silently (https://github.com/rails/rails/issues/17622), meaning that
    the hook was called on every save without reporting an error.
    
    With Rails 6 on: now generates an
    error (https://github.com/rails/rails/pull/30919) instead of failing silently.
    
    As a result, the email downcase hook has been rewritten to use if: instead.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -14,7 +14,7 @@ class Invite < ActiveRecord::Base
   end
 
   before_create :generate_token
-  before_save :set_email_case, on: :create
+  before_save :set_email_case, if: :email_changed?
   before_save :check_recipient_existence
 
   validates :email, presence: true
